e62c492bea5f641e28351012a2a5c050060d805a
Bushra-B <bushraxbil@gmail.com>
2020-04-18 22:04:56 +0300

    create dashboard form

---
+++ public/script/test.js
@@ -0,0 +1,6 @@
+// const summary = require('../../server.js');
+// require('test2');
+// console.log(arr);
+
+console.log(summary);
+
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const pg = require('pg');
 const superagent = require('superagent');
 const methodOverRide = require('method-override');
+const path = require('path');
 
 //==========(main variebles)===========\\
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(methodOverRide('_method'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 //==========(all routes)===========\\
@@ -33,15 +35,20 @@ app.get('/dashboard', dashboardHandler);
 function homepageHandler(request, response) {
   response.render('./index');
 }
-
+let moduleObj = {};
 // Dashboard handler
 function dashboardHandler(request, response) {
   let url = 'https://api.covid19api.com/summary';
   superagent.get(url)
     .then(summaryData => {
       let xmlData = JSON.parse(summaryData.text);
+      console.log('ddddd', xmlData.Countries);
+      let countryData = xmlData.Countries.map(element => {
+        return new CounrtySummary(element);
+      });
       let summaryObj = new Summary(xmlData.Global);
-      response.render('./pages/covid19Dashboard', {summary: summaryObj});
+      moduleObj.summary = summaryObj;
+      response.render('./pages/covid19Dashboard', {summary: summaryObj, countries: countryData});
     });
 }
 // C.F
@@ -53,7 +60,13 @@ function Summary(data) {
   this.NewRecovered =	data.NewRecovered;
   this.TotalRecovered =	data.TotalRecovered;
 }
-
+function CounrtySummary(data) {
+  this.Country = data.Country;
+  this.TotalConfirmed = data.TotalConfirmed;
+  this.TotalDeaths = data.TotalDeaths;
+  this.TotalRecovered = data.TotalRecovered;
+}
+// module.exports = moduleObj;
 //==========(error handlers)===========\\
 function errorHandler(err, req, res) {
   res.status(500).send(err);
