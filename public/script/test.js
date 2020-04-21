c231a474741d879c72d9a7962a286af66066de37
israaothman <israaayoub@hotmail.com>
2020-04-22 00:43:31 +0300

    done the forms

--- public/script/test.js
+++ public/script/test.js
@@ -202,3 +202,17 @@ runderChartForSingleCo();
 $(document).ready( function () {
   $('#myTable').DataTable();
 } );
+
+
+//=============global sammary===========\\
+
+$('.Count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 1000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
