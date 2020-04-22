e77db04e9bcf8de322ab73128e741f91e5cd9828
israaothman <israaayoub@hotmail.com>
2020-04-22 21:32:29 +0300

    for Osama

--- public/script/header.js
+++ public/script/header.js
@@ -7,11 +7,11 @@ $('#toggle').click(function () {
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   if (scroll > 300) {
-    $(".default-color").css("background-color", "#20202083");
+    $('.default-color').css('background-color', '#20202083');
   }
 
   else if (scroll === 0) {
-    $(".default-color").css("background-color", "#202020");
+    $('.default-color').css('background-color', '#202020');
   }
 });
 
@@ -19,9 +19,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById('navbar').style.top = '0';
   } else {
-    document.getElementById("navbar").style.top = "-110px";
+    document.getElementById('navbar').style.top = '-110px';
   }
   prevScrollpos = currentScrollPos;
 };
@@ -30,11 +30,43 @@ window.onscroll = function () {
 // ================(read more button)==================\\
 
 $('#readmore').on('click', () => {
-  $('#read').toggleClass()
+  $('#read').toggleClass();
   if ($('#readmore').text() === 'Show Less'){
-    $('#readmore').text('Show More')
+    $('#readmore').text('Show More');
   }else{
-    $('#readmore').text('Show Less')
+    $('#readmore').text('Show Less');
   }
-  
-})
+
+});
+
+
+
+
+// ============================
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+
+
+  if (scroll > 200) {
+    $('#desktop-nav').css('height', '2em');
+    $('#desktop-nav').css('background', '#202020ab');
+    $('#desktop-nav').css('box-shadow', '6px 6px 6px #1d1c1c91');
+
+  }
+
+  else if (scroll === 0) {
+    $('#desktop-nav').css('height', '4.82em');
+    $('#desktop-nav').css('background', '#02081E');
+    $('#desktop-nav').css('box-shadow', 'none');
+  }
+
+});
+prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('desktop-nav').style.top = '0';
+    prevScrollpos = currentScrollPos;
+  }};
+
